876ec55ba5da1a5b45559b7b42abc127b191e323
tychay <tychay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-23 22:52:14 +0000

    Fix the look of the form
    Now in a table to resemble a lot of popular plugins
    
    git-svn-id: http://plugins.svn.wordpress.org/one-click-child-theme/trunk@1119233 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- panel.php
+++ panel.php
@@ -19,26 +19,31 @@ endif;
 ?>
 
 	<form action="<?php echo admin_url( 'themes.php?page=one-click-child-theme-page' ); ?>" method="post" id="create_child_form">
-		<p>
-			<label>
-				<?php _e( 'Give your theme a name:', 'one-click-child-theme' ) ?>
-				<input type="text" name="theme_name" value="<?php echo $theme_name; ?>" />
-			</label>
-		</p>
-		<p>
-			<label>
-				<?php _e( 'Describe your theme:', 'one-click-child-theme' ) ?><br />
-				<textarea name="description" value="<?php echo $description; ?>" rows="2" cols="40"/></textarea>
-			</label>
-		</p>
-		<p>
-			<label>
-				<?php _e( 'Your Name:', 'one-click-child-theme' ) ?>
-				<input name="author_name" value="<?php echo $author; ?>" />
-			</label>
-		</p>
+		<table class="form-table">
+			<tr>
+				<th scope="row"><label for="create_child_theme_name"><?php _e( 'Theme Name', 'one-click-child-theme' ); ?></label></th>
+				<td>
+					<input type="text" name="theme_name" value="<?php echo $theme_name; ?>" id="create_child_theme_name" />
+					<p class="description"><?php _e( 'Give your theme a name.', 'one-click-child-theme' ); ?>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row"><label for="create_child_description"><?php _e( 'Description', 'one-click-child-theme' ); ?></label></th>
+				<td>
+					<textarea name="description" value="<?php echo $description; ?>" rows="2" cols="40" id="create_child_description"></textarea>
+					<p class="description"><?php _e( 'Describe your theme.', 'one-click-child-theme' ); ?>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row"><label for="create_child_author_name"><?php _e( 'Author', 'one-click-child-theme' ); ?></label></th>
+				<td>
+					<input name="author_name" value="<?php echo $author; ?>" id="create_child_author_name" />
+					<p class="description"><?php _e( 'Your name.', 'one-click-child-theme' ); ?>
+				</td>
+			</tr>
+		</table>
 		<p class="submit">
-			<input type="submit" class="button-primary" value="<?php _e( 'Create Child', 'one-click-child-theme' ); ?>" />
+			<input type="submit" class="button button-primary" value="<?php _e( 'Create Child', 'one-click-child-theme' ); ?>" />
 		</p>
 	</form>
 </div>
